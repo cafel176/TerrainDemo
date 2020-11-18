84164aa102aaa68bb39c369b7ced6d70aa9d0abe
ZHT <zhanghaitaoty@163.com>
2020-11-18 10:48:55 +0000

    a

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+/*
+ * 类、方法名、public变量、常量、枚举名和枚举值 所有单词开头字母大写(Pascal规则)
+ * protected/private变量、方法参数 都是开头字母小写，第二个单词开始大写(camel规则)
+ */
+
+/* 
+ * 所有的UI操作都要屏蔽相机操作
+ * 尽量少在update中操作
+ * 点选物体和相机旋转操作冲突(待办)
+ * 全屏幕透明Image，使用UI的鼠标拖拽IDragHandler，滚轮接口实现类似效果，鼠标操作在接口函数实现，可以把update空出来
+ */
+
+// 相机模式
+public enum CameraType
+{
+    Free,
+    Focus
+}
+
+// 聚焦模式下取消选中物体的方式
+public enum DoType
+{
+    Mouse,
+    Button
+}
+
+public class CameraControl : MonoBehaviour {
+
+    // 控制平移速度
+    public float MoveSpeed = 2.0f;
+    // 控制旋转速度
+    public float RotateSpeed = 2.0f;
+    // 当前相机模式
+    public CameraType NowType = CameraType.Free;
+    // 聚焦模式下聚焦中心的物体
+    public GameObject FocusObject=null;
+    // 聚焦模式下如何取消选中
+    public DoType NowDoType = DoType.Mouse;
+
+    // 当前操纵的相机
+    private Camera mainCamera = null;
+    // 记录自由模式当前的位置和方向
+    private Vector3 nowPos, nowLookAt;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+
+        // 记录当前相机位置和方向
+        nowPos = mainCamera.transform.position;
+        nowLookAt = mainCamera.transform.forward + mainCamera.transform.position;
+
+        if (NowType == CameraType.Focus)
+        {
+            SetFocusObject(FocusObject, new Vector3(-3, 3, -3));
+        }
+    }
+
+    void Update ()
+    {
+        // 点击UI时不触发射线
+        if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        // 获取鼠标输入
+        float _mouseX = Input.GetAxis("Mouse X");
+        float _mouseY = Input.GetAxis("Mouse Y");
+
+        // 没有聚焦物体，自由模式
+        if (FocusObject==null)
+        {
+            // 记录当前相机位置和方向
+            nowPos = mainCamera.transform.position;
+            nowLookAt = mainCamera.transform.forward + mainCamera.transform.position;
+
+            if (NowDoType == DoType.Mouse)
+            {
+                // 点击选择物体并进入聚焦模式
+                if (Input.GetMouseButtonDown(0))
+                {
+                    var ray = mainCamera.ScreenPointToRay(Input.mousePosition);//鼠标的屏幕坐标转化为一条射线
+                    RaycastHit hit;
+
+                    //屏蔽mask层
+                    int layerMask = 1 << 8;
+                    layerMask = ~layerMask;
+                    // 射线距离1000
+                    if (Physics.Raycast(ray, out hit, 1000, layerMask))
+                    {
+                        var g = hit.collider.gameObject;
+                        SetFocusObject(g, new Vector3(-3, 3, -3));
+                    }
+                }
+            }
+
+            CameraRotate(_mouseX, _mouseY);
+            CameraMove();
+        }
+        else// 聚焦模式
+        {
+            // 如果取消模式是鼠标
+            if (NowDoType == DoType.Mouse)
+            {
+                // 点击切换聚焦物体或取消聚焦模式
+                if (Input.GetMouseButtonDown(0))
+                {
+                    var ray = mainCamera.ScreenPointToRay(Input.mousePosition);//鼠标的屏幕坐标转化为一条射线
+                    RaycastHit hit;
+
+                    // 射线距离1000
+                    if (Physics.Raycast(ray, out hit,1000))
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+
+            CameraRotateFocus(_mouseX, _mouseY);
+            CameraMoveFocus(_mouseX, _mouseY);
+        }
+
+        // 滚轮缩放
+        CameraScale();
+    }
+
+    // 设置聚焦物体并切换模式，由UI调用
+    public void SetFocusObject(GameObject g, Vector3 pos)
+    {
+        GameObject pre = FocusObject;
+        if(g!=null)// 物体存在，聚焦模式
+        {
+            FocusObject = g;
+            mainCamera.transform.position = FocusObject.transform.position + pos;
+            mainCamera.transform.LookAt(FocusObject.transform);
+        }
+        else// 物体不存在，取消聚焦模式(自由模式)
+        {
+            FocusObject = null;
+            mainCamera.transform.position = nowPos;
+            mainCamera.transform.LookAt(nowLookAt);
+        }
+    }
+
+    // 获取当前的聚焦物体
+    public GameObject GetFocusObject()
+    {
+        return FocusObject;
+    }
+
+    // 自由模式下相机旋转
+    private void CameraRotate(float _mouseX, float _mouseY)
+    {
+        mainCamera.transform.Rotate(Vector3.up, _mouseX * RotateSpeed, Space.World);
+        mainCamera.transform.Rotate(transform.right, -_mouseY * RotateSpeed, Space.World);
+    }
+
+    // 自由模式下相机平移
+    private void CameraMove()
+    {
+        float value = 0;
+        //上下移动
+        if (Input.GetKey(KeyCode.Z))
+            value = 1;
+        else if (Input.GetKey(KeyCode.X))
+            value = -1;
+        //前后左右移动
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), value, Input.GetAxis("Vertical"));
+        mainCamera.transform.Translate(moveDirection * Time.deltaTime * MoveSpeed, Space.Self);
+    }
+
+    // 聚焦模式下相机旋转
+    private void CameraRotateFocus(float _mouseX, float _mouseY)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mainCamera.transform.RotateAround(FocusObject.transform.position, Vector3.up, _mouseX * RotateSpeed);
+            mainCamera.transform.RotateAround(FocusObject.transform.position, mainCamera.transform.right, -_mouseY * RotateSpeed);
+        }
+    }
+
+    // 聚焦模式下相机平移
+    private void CameraMoveFocus(float _mouseX, float _mouseY)
+    {
+        if (Input.GetMouseButton(2))
+        {
+            Vector3 moveDir = (_mouseX * -mainCamera.transform.right + _mouseY * -mainCamera.transform.up);
+            mainCamera.transform.position += moveDir * MoveSpeed *0.25f;
+        }
+    }
+
+    // 滚轮缩放
+    private void CameraScale()
+    {
+        //放大
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+             mainCamera.transform.position -= 0.5f* mainCamera.transform.forward;
+        }
+        //缩小
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            mainCamera.transform.position += 0.5f * mainCamera.transform.forward;
+        }
+    }
+}
---
+++ Assets/Scripts/TerrainLoader.cs
@@ -0,0 +1,330 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public enum LimitType
+{
+    Sphere,
+    Rect
+}
+
+public class TerrainLoader : MonoBehaviour
+{
+    // 需要带后缀名
+    public string HeightDataFile;
+    // 显示全部数据时的地形显示尺寸
+    public int MapSize;
+    // 原始数据的行列数
+    public int DataSize;
+    //中心点写经纬度，限制范围单位公里
+    public Vector2 Center,Limit;
+    // 外圈范围粗细和其高度值
+    public float OutWidth, OutValue;
+    // 查询方式
+    public LimitType limit;
+
+    public Terrain terrain;
+    public InputField CX, CY, RX, RY;
+
+    private string[] file = null;
+    private float detailHeight = 0;
+    private float detailWidth = 0;
+    private float detailLength = 0;
+    private float min = float.PositiveInfinity;
+    private float minX = float.PositiveInfinity;
+    private float minY = float.PositiveInfinity;
+    private float max = 0;
+    private float maxX = 0;
+    private float maxY = 0;
+
+    private float epsilon = 1e-8f;
+    private float EarthRadiusKm = 6378.137f; // WGS-84
+
+    private void Start()
+    {
+        if(terrain!=null)
+        {
+            terrain.gameObject.SetActive(true);
+            terrain.basemapDistance = 1000000;
+        }          
+
+        CX.text = Center.x.ToString();
+        CY.text = Center.y.ToString();
+        RX.text = Limit.x.ToString();
+        RY.text = Limit.y.ToString();
+
+        CX.onValueChanged.AddListener(delegate(string t) {
+            var a = float.Parse(CX.text);
+            if (a > epsilon)
+                Center.x = a;
+        });
+        CY.onValueChanged.AddListener(delegate (string t) {
+            var a = float.Parse(CY.text);
+            if (a > epsilon)
+                Center.y = a;
+        });
+        RX.onValueChanged.AddListener(delegate (string t) {
+            var a = float.Parse(RX.text);
+            if (a > epsilon)
+                Limit.x = a;
+        });
+        RY.onValueChanged.AddListener(delegate (string t) {
+            var a = float.Parse(RY.text);
+            if (a > epsilon)
+                Limit.y = a;
+        });
+    }
+
+    public void LoadFile(string filename)
+    {
+        // 读取资源
+        string[] fileContents = File.ReadAllLines(Application.dataPath + @"/Data/" + filename);
+        file = new string[fileContents.Length];
+
+        for (int i = 0; i < fileContents.Length; i++)
+        {
+            file[i] = fileContents[i];
+
+            string[] txt = fileContents[i].Split(',');
+            var x = float.Parse(txt[2]);
+            var y = float.Parse(txt[3]);
+            var h = float.Parse(txt[4]);
+
+            if (minX > x)
+                minX = x;
+            if (maxX < x)
+                maxX = x;
+
+            if (minY > y)
+                minY = y;
+            if (maxY < y)
+                maxY = y;
+
+            if (min > h)
+                min = h;
+            if (max < h)
+                max = h;
+        }
+
+        // 计算出地形最大值与最小值的高度差
+        detailHeight = max - min;
+        detailWidth = maxX - minX;
+        detailLength = maxY - minY;
+    }
+
+    public void Spawn()
+    {
+        if(file == null)
+        {
+            LoadFile(HeightDataFile);
+        }
+
+        float[,] data = new float[DataSize, DataSize];
+
+        // 填数据
+        int startX = int.MaxValue, startY = int.MaxValue, endX = 0, endY = 0;
+        int sizeX = 0, sizeY = 0;
+        for (int i = 0; i < file.Length; i++)
+        {
+            string[] txt = file[i].Split(',');
+            var x = float.Parse(txt[2]);
+            var y = float.Parse(txt[3]);
+            var h = float.Parse(txt[4]);
+
+            if (!InLimit(x, y, OutWidth))
+                continue;
+            else 
+            {
+                float a1 = (x - minX) / detailWidth * (DataSize - 1);
+                float b1 = (y - minY) / detailLength * (DataSize - 1);
+                int a = RoundToInt(a1);
+                int b = RoundToInt(b1);
+
+                if (!InLimit(x, y))
+                {
+                    data[a, b] = OutValue;
+                }
+                else
+                {
+                    data[a, b] = h;
+                }
+
+                if (startX > a)
+                    startX = a;
+                if (endX < a)
+                    endX = a;
+
+                if (startY > b)
+                    startY = b;
+                if (endY < b)
+                    endY = b;
+            }                
+        }
+        sizeX = endX - startX + 1;
+        sizeY = endY - startY + 1;
+        if (endX==int.MaxValue)
+            sizeX = 0;
+        if (endY == int.MaxValue)
+            sizeY = 0;
+
+        // 0数据的修复，周围8个点取平均
+        for (int a = 0; a < sizeX; a++)
+        {
+            for (int b = 0; b < sizeY; b++)
+            {
+                if (Mathf.Abs(data[a+startX, b+startY]) > epsilon)
+                    continue;
+
+                float t = 0;int num = 0;
+                {
+                    if (a < sizeX - 1 && Mathf.Abs(data[a + 1 + startX, b + startY]) > epsilon)
+                    {
+                        t += data[a + 1 + startX, b + startY];
+                        num++;
+                    }
+                    if (a >= 1 && Mathf.Abs(data[a - 1 + startX, b + startY]) > epsilon)
+                    {
+                        t += data[a - 1 + startX, b + startY];
+                        num++;
+                    }
+                }
+                if (b<sizeY-1)
+                {
+                    if(Mathf.Abs(data[a + startX, b + 1 + startY]) > epsilon)
+                    {
+                        t += data[a + startX, b + 1 + startY];
+                        num++;
+                    }
+                    if (a < sizeX - 1 && Mathf.Abs(data[a + 1 + startX, b + 1 + startY]) > epsilon)
+                    {
+                        t += data[a + 1 + startX, b + 1 + startY];
+                        num++;
+                    }
+                    if (a >= 1 && Mathf.Abs(data[a - 1 + startX, b + 1 + startY]) > epsilon)
+                    {
+                        t += data[a - 1 + startX, b + 1 + startY];
+                        num++;
+                    }
+                }
+                if (b >= 1)
+                {
+                    if(Mathf.Abs(data[a + startX, b - 1 + startY]) > epsilon)
+                    {
+                        t += data[a + startX, b - 1 + startY];
+                        num++;
+                    }
+                    if (a < sizeX - 1 && Mathf.Abs(data[a + 1 + startX, b - 1 + startY]) > epsilon)
+                    {
+                        t += data[a + 1 + startX, b - 1 + startY];
+                        num++;
+                    }
+                    if (a >= 1 && Mathf.Abs(data[a - 1 + startX, b - 1 + startY]) > epsilon)
+                    {
+                        t += data[a - 1 + startX, b - 1 + startY];
+                        num++;
+                    }
+                }
+                if(num>4)
+                    data[a + startX, b + startY] = t / num;
+            }
+        }
+
+        // 将地形整体上移，消灭负值
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                if (min < 0)
+                {
+                    data[i + startX, j + startY] = data[i+startX, j + startY] - min;
+                }
+                data[i + startX, j + startY] = data[i + startX, j + startY] / detailHeight;
+            }
+        }
+
+        // 设置地形的最大高度
+        terrain.terrainData.size = new Vector3(MapSize, detailHeight, MapSize);
+        // 高度图分辨率
+        terrain.terrainData.heightmapResolution = DataSize;
+        terrain.terrainData.alphamapResolution = DataSize;
+        // 导入高度数据
+        terrain.terrainData.SetHeights(0, 0, data);
+        float[,,] w = new float[terrain.terrainData.alphamapWidth, terrain.terrainData.alphamapHeight, 2];
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                if(Mathf.Abs(data[x + startX, y + startY]) > epsilon)
+                {
+                    w[x + startX, y + startY, 0] = 1f;
+                    w[x + startX, y + startY, 1] = 0f;
+                }
+                else
+                {
+                    w[x + startX, y + startY, 0] = 0f;
+                    w[x + startX, y + startY, 1] = 1f;
+                }               
+            }
+        }
+        terrain.terrainData.SetAlphamaps(0, 0, w);
+    }
+
+    // 计算某点是否在范围内
+    private bool InLimit(float x, float y, float extra = 0)
+    {
+        bool a = false;
+        if(limit == LimitType.Sphere)
+            a= InLimitSphere(x, y, extra);
+        else if (limit == LimitType.Rect)
+            a = InLimitRect(x, y, extra);
+        return a;
+    }
+
+    // 计算某点是否在矩形范围内
+    private bool InLimitRect(float x,float y,float extra)
+    {
+        if (GetDistance(x, y, Center.x, y) <= Limit.x+ extra && GetDistance(x, y, x, Center.y) <= Limit.y + extra)
+            return true;
+
+        return false;
+    }
+
+    // 计算某点是否在圆形范围内
+    private bool InLimitSphere(float x, float y, float extra)
+    {
+        if (GetDistance(x, y, Center.x, Center.y) <= Limit.x+ extra)
+            return true;
+
+        return false;
+    }
+
+    // 通过经纬度算距离，lat纬度，lng经度
+    private float GetDistance(float p1Lat, float p1Lng, float p2Lat, float p2Lng)
+    {
+
+        float dLat1InRad = p1Lat * (Mathf.PI / 180);
+        float dLong1InRad = p1Lng * (Mathf.PI / 180);
+        float dLat2InRad = p2Lat * (Mathf.PI / 180);
+        float dLong2InRad = p2Lng * (Mathf.PI / 180);
+
+        float dLongitude = dLong2InRad - dLong1InRad;
+        float dLatitude = dLat2InRad - dLat1InRad;
+
+        float a = Mathf.Pow(Mathf.Sin(dLatitude / 2), 2) + Mathf.Cos(dLat1InRad) * Mathf.Cos(dLat2InRad) * Mathf.Pow(Mathf.Sin(dLongitude / 2), 2);
+        float c = 2 * Mathf.Atan2(Mathf.Sqrt(a), Mathf.Sqrt(1 - a));
+
+        return EarthRadiusKm * c;
+    }
+
+    // 四舍五入
+    private int RoundToInt(float value)
+    {
+        int a = (int)value;
+        if (value - a >= 0.5f)
+            return a + 1;
+        else
+            return a;
+    }
+}
